cec42398e0a953f3d2de6078965580aa70f2f771
johndannie6-dotcom <johndannie6@gmail.com>
2026-03-25 01:09:35 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -6,8 +6,18 @@ const app = express();
 app.use(express.json());
 app.use(express.static(__dirname));
 
-// 🔥 SIMPLE & WORKING ATLAS CONNECTION
-mongoose.connect("mongodb://admin:admin123@cluster0-shard-00-00.2gb8wnq.mongodb.net:27017,cluster0-shard-00-01.2gb8wnq.mongodb.net:27017,cluster0-shard-00-02.2gb8wnq.mongodb.net:27017/portfolioDB?ssl=true&replicaSet=atlas-2gb8wnq-shard-0&authSource=admin&retryWrites=true&w=majority")
+
+async function connectDB() {
+  try {
+    await mongoose.connect("mongodb://admin:admin123@cluster0-shard-00-00.2gb8wnq.mongodb.net:27017,cluster0-shard-00-01.2gb8wnq.mongodb.net:27017,cluster0-shard-00-02.2gb8wnq.mongodb.net:27017/portfolioDB?ssl=true&replicaSet=atlas-2gb8wnq-shard-0&authSource=admin&retryWrites=true&w=majority");
+
+    console.log("MongoDB connected");
+  } catch (err) {
+    console.log("MongoDB ERROR:", err);
+  }
+}
+
+connectDB();
 .then(() => console.log("MongoDB connected"))
 .catch(err => console.log(err));
 
